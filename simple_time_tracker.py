203f2986de1f2525a3b2507991708bb37152f04c
Maximilian Ernestus <maximilian@ernestus.de>
2023-02-05 02:15:04 +0100

    Fix bug in most_recent_worked_tasks_and_clients.

--- simple_time_tracker.py
+++ simple_time_tracker.py
@@ -94,7 +94,7 @@ class WorkedTimeStore:
         :return: A generator yielding the most recent n task-client-tuples.
         """
         work_items = set()
-        for worked_time in self.worked_times():
+        for worked_time in reversed(self.worked_times()):
             work_item = (worked_time.task, worked_time.client)
             if work_item not in work_items:
                 work_items.add(work_item)
