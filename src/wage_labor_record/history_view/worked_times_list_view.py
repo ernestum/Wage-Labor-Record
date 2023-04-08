d701126f2cc3ce6f919c24dbc471bc1fcb9ffda9
Maximilian Ernestus <maximilian@ernestus.de>
2023-04-08 09:33:30 +0200

    Refactor worked times list view and add delete button.

--- src/wage_labor_record/history_view/worked_times_list_view.py
+++ src/wage_labor_record/history_view/worked_times_list_view.py
@@ -37,54 +37,95 @@ class WorkedTimesListView(Gtk.ListBox):
         box.show()
         row.add(box)
 
-        task_label = Gtk.Entry(
-            text=item.task,
-            placeholder_text="Task",
-            completion=make_completer(self._worked_time_store.tasks),
-        )
-        task_label.set_has_frame(False)
-        def set_task(*args):
-            item.task = task_label.get_text()
-        task_label.connect("activate", set_task)
-        # item.bind_property("task", task_label, "text", GObject.BindingFlags.BIDIRECTIONAL | GObject.BindingFlags.SYNC_CREATE)
-        task_label.show()
-        box.pack_start(task_label, True, True, 0)
+        box.pack_start(self._create_task_entry(item), True, True, 0)
+        box.pack_start(self._create_client_entry(item), True, True, 0)
+        box.pack_start(self._create_start_time_button(item), True, True, 0)
+        box.pack_start(self._create_end_time_button(item), True, True, 0)
+        box.pack_start(self._create_delete_button(item), True, True, 0)
+        row.show()
+        return row
 
-        client_label = Gtk.Entry(
-            text=item.client,
-            placeholder_text="Client",
-            completion=make_completer(self._worked_time_store.clients),
-        )
-        client_label.set_has_frame(False)
-        def set_client(*args):
-            item.client = client_label.get_text()
-        client_label.connect("activate", set_client)
-        # item.bind_property("client", client_label, "text", GObject.BindingFlags.BIDIRECTIONAL | GObject.BindingFlags.SYNC_CREATE)
-        client_label.show()
-        box.pack_start(client_label, True, True, 0)
-
-        start_time_label = Gtk.Button()
-        start_time_label.set_relief(Gtk.ReliefStyle.NONE)
+    def _create_start_time_button(self, item):
+        start_time_button = Gtk.Button()
+        start_time_button.set_relief(Gtk.ReliefStyle.NONE)
 
         def on_start_time_clicked(*args):
             picker = DatetimePicker(item.start_time)
 
             picker.connect("datetime-changed", lambda picker, time: item.set_property("start-time", time))
-            picker.set_relative_to(start_time_label)
+            picker.set_relative_to(start_time_button)
             picker.show_all()
             picker.popup()
 
         def on_start_time_changed(*args):
-            start_time_label.set_label(self._get_start_time_string(item))
+            start_time_button.set_label(self._get_start_time_string(item))
+
         on_start_time_changed()
         item.connect("notify::start-time", on_start_time_changed)
+        start_time_button.connect("clicked", on_start_time_clicked)
+        start_time_button.show()
+        return start_time_button
 
-        start_time_label.connect("clicked", on_start_time_clicked)
-        start_time_label.show()
-        box.pack_start(start_time_label, True, True, 0)
+    def _create_end_time_button(self, item):
+        end_time_label = Gtk.Button()
+        end_time_label.set_relief(Gtk.ReliefStyle.NONE)
 
-        row.show()
-        return row
+        def on_end_time_clicked(*args):
+            picker = DatetimePicker(item.end_time)
+
+            picker.connect("datetime-changed", lambda picker, time: item.set_property("end-time", time))
+            picker.set_relative_to(end_time_label)
+            picker.show_all()
+            picker.popup()
+
+        def on_end_time_changed(*args):
+            end_time_label.set_label(self._get_end_time_string(item))
+
+        on_end_time_changed()
+        item.connect("notify::end-time", on_end_time_changed)
+        end_time_label.connect("clicked", on_end_time_clicked)
+        end_time_label.show()
+        return end_time_label
+
+    def _create_client_entry(self, item: WorkedTime):
+        client_entry = Gtk.Entry(
+            text=item.client,
+            placeholder_text="Client",
+            completion=make_completer(self._worked_time_store.clients),
+        )
+        client_entry.set_has_frame(False)
+
+        def set_client(*args):
+            item.client = client_entry.get_text()
+
+        client_entry.connect("activate", set_client)
+        # item.bind_property("client", client_label, "text", GObject.BindingFlags.BIDIRECTIONAL | GObject.BindingFlags.SYNC_CREATE)
+        client_entry.show()
+        return client_entry
+
+    def _create_task_entry(self, item: WorkedTime):
+        task_entry = Gtk.Entry(
+            text=item.task,
+            placeholder_text="Task",
+            completion=make_completer(self._worked_time_store.tasks),
+        )
+        task_entry.set_has_frame(False)
+
+        def set_task(*args):
+            item.task = task_entry.get_text()
+
+        task_entry.connect("activate", set_task)
+        # item.bind_property("task", task_label, "text", GObject.BindingFlags.BIDIRECTIONAL | GObject.BindingFlags.SYNC_CREATE)
+        task_entry.show()
+        return task_entry
+
+    def _create_delete_button(self, item: WorkedTime):
+        delete_button = Gtk.Button()
+        delete_button.set_relief(Gtk.ReliefStyle.NONE)
+        delete_button.set_image(Gtk.Image.new_from_icon_name("edit-delete-symbolic", Gtk.IconSize.BUTTON))
+        delete_button.connect("clicked", lambda *args: self._worked_time_store.remove_item(item))
+        delete_button.show()
+        return delete_button
 
     def _get_start_time_string(self, item: WorkedTime) -> str:
         if self._all_items_in_same_day:
